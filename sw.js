41a09d6405b5a6516385bf046ded5a5b6208854d
lain2983-commits <lain2983@gmail.com>
2026-05-20 22:21:59 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='seobun-hx-v38';
+const ASSETS=['/0089/','/0089/index.html','/0089/manifest.json','/0089/icon-192.png','/0089/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
